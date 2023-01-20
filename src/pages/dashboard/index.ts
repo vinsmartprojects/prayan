518e5ff7a29d8de28568ed98a03c8e312cb5da01
SARASWATI <vinsmart.projects@gmail.com>
2023-01-20 14:58:23 +0530

    Updated

--- src/pages/dashboard/index.ts
+++ src/pages/dashboard/index.ts
@@ -1,16 +1,6 @@
-import { useEffect } from 'react';
-import { useRouter } from 'next/router';
 
 // ----------------------------------------------------------------------
 
 export default function Index() {
-  const router = useRouter();
-
-  useEffect(() => {
-    if (router.pathname === '/dashboard') {
-      router.push('/dashboard/one');
-    }
-  });
-
   return null;
 }
