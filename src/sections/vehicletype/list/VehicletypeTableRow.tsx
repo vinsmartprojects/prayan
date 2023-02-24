bff38a7e42cc28691ecc2c3425500d25e6f4bc23
vinsmartprojects <110082062+vinsmartprojects@users.noreply.github.com>
2023-02-24 23:36:24 +0530

    Update VehicletypeTableRow.tsx

--- src/sections/vehicletype/list/VehicletypeTableRow.tsx
+++ src/sections/vehicletype/list/VehicletypeTableRow.tsx
@@ -68,10 +68,10 @@ export default function VehicletypeTableRow({
         <TableCell padding="checkbox">
           <Checkbox checked={selected} onClick={onSelectRow} />
         </TableCell>
-        <TableCell   align="left">
+        <TableCell   align="left"  onClick={onDetailRow}>
           <Avatar alt={name} src={cdnPath(image)} />
         </TableCell>
-        <TableCell padding="checkbox" align="left">
+        <TableCell padding="checkbox" align="left"  onClick={onDetailRow}>
         <Typography variant="subtitle2" noWrap>
               {id}
             </Typography>
