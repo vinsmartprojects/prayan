39a37d52544e47878e543e40f494529676f01b64
Chethan Sreenivasamurthy <chethan.praveen@gmail.com>
2023-03-11 15:47:04 +0530

    popup dialoug for all three vehicle settings

--- src/sections/vehicle/settings/maker/MakerContainer.tsx
+++ src/sections/vehicle/settings/maker/MakerContainer.tsx
@@ -1,7 +1,21 @@
 import React from 'react';
 import {
+  SelectChangeEvent,
+  MenuItem,
+  InputLabel,
+  FormControlLabel,
+  DialogTitle,
+  DialogContentText,
+  DialogContent,
+  DialogActions,
+  FormControl,
+  Dialog,
+  DialogProps,
+  Switch,
+  Select,
   Box,
   Paper,
+  Typography,
   Grid,
   Tab,
   Tabs,
@@ -17,36 +31,104 @@ import {
 } from '@mui/material';
 import { useSettingsContext } from 'src/components/settings';
 
-const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-  ...theme.typography.body2,
-  padding: theme.spacing(1),
-  textAlign: 'center',
-  color: theme.palette.text.secondary,
-}));
 type Props = {};
-function MakerContainer({}: Props) {
-  const { themeStretch } = useSettingsContext();
-  return (
-    <>
-      <head>
-        <title>Vehicle - Maker</title>
-      </head>
+export default function MakerContainer({}: Props) {
+  const [open, setOpen] = React.useState(false);
+  const [fullWidth, setFullWidth] = React.useState(true);
+  const [maxWidth, setMaxWidth] = React.useState<DialogProps['maxWidth']>('sm');
 
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleMaxWidthChange = (event: SelectChangeEvent<typeof maxWidth>) => {
+    setMaxWidth(
+      // @ts-expect-error autofill of arbitrary value is not handled.
+      event.target.value
+    );
+  };
+
+  const handleFullWidthChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setFullWidth(event.target.checked);
+  };
+  const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  }));
+  const { themeStretch } = useSettingsContext();
+  type Props = {};
+  return (
+    <React.Fragment>
       <Container maxWidth={themeStretch ? false : 'lg'}>
-        <Grid container spacing={2}>
-          <Grid item xs={6} md={8}>
-            <Item sx={{ fontSize: 20 }}>Vehicle Maker List</Item>
-          </Grid>
-          <Grid item xs={6} md={4}>
-            <Item>
-              <Button variant="contained">New Maker</Button>
-            </Item>
-          </Grid>
-        </Grid>
+        <Box sx={{ display: 'flex' }}>
+          <Box sx={{ flexGrow: 1 }}>
+            <Typography variant="h4" component="h4">
+              Vehicle Maker
+            </Typography>
+          </Box>
+
+          <Box sx={{}}>
+            <Button variant="outlined" onClick={handleClickOpen}>
+              Add Maker
+            </Button>
+          </Box>
+        </Box>
       </Container>
-    </>
+
+      <Dialog fullWidth={fullWidth} maxWidth={maxWidth} open={open} onClose={handleClose}>
+        <DialogTitle>Optional sizes</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            You can set my maximum width and whether to adapt or not.
+          </DialogContentText>
+          <Box
+            noValidate
+            component="form"
+            sx={{
+              display: 'flex',
+              flexDirection: 'column',
+              m: 'auto',
+              width: 'fit-content',
+            }}
+          >
+            <FormControl sx={{ mt: 2, minWidth: 120 }}>
+              <InputLabel htmlFor="max-width">maxWidth</InputLabel>
+              <Select
+                autoFocus
+                value={maxWidth}
+                onChange={handleMaxWidthChange}
+                label="maxWidth"
+                inputProps={{
+                  name: 'max-width',
+                  id: 'max-width',
+                }}
+              >
+                <MenuItem value={false as any}>false</MenuItem>
+                <MenuItem value="xs">xs</MenuItem>
+                <MenuItem value="sm">sm</MenuItem>
+                <MenuItem value="md">md</MenuItem>
+                <MenuItem value="lg">lg</MenuItem>
+                <MenuItem value="xl">xl</MenuItem>
+              </Select>
+            </FormControl>
+            <FormControlLabel
+              sx={{ mt: 1 }}
+              control={<Switch checked={fullWidth} onChange={handleFullWidthChange} />}
+              label="Full width"
+            />
+          </Box>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Close</Button>
+        </DialogActions>
+      </Dialog>
+    </React.Fragment>
   );
 }
-
-export default MakerContainer;
--- src/sections/vehicle/settings/models/ModelsContainer.tsx
+++ src/sections/vehicle/settings/models/ModelsContainer.tsx
@@ -1,8 +1,21 @@
 import React from 'react';
 import {
-  Typography,
+  SelectChangeEvent,
+  MenuItem,
+  InputLabel,
+  FormControlLabel,
+  DialogTitle,
+  DialogContentText,
+  DialogContent,
+  DialogActions,
+  FormControl,
+  Dialog,
+  DialogProps,
+  Switch,
+  Select,
   Box,
   Paper,
+  Typography,
   Grid,
   Tab,
   Tabs,
@@ -18,6 +31,31 @@ import {
 } from '@mui/material';
 import { useSettingsContext } from 'src/components/settings';
 
+
+type Props = {};
+export default function ModelsContainer({}: Props) {
+  const [open, setOpen] = React.useState(false);
+  const [fullWidth, setFullWidth] = React.useState(true);
+  const [maxWidth, setMaxWidth] = React.useState<DialogProps['maxWidth']>('sm');
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleMaxWidthChange = (event: SelectChangeEvent<typeof maxWidth>) => {
+    setMaxWidth(
+      // @ts-expect-error autofill of arbitrary value is not handled.
+      event.target.value
+    );
+  };
+
+  const handleFullWidthChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setFullWidth(event.target.checked);
+  };
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
   ...theme.typography.body2,
@@ -25,30 +63,73 @@ const Item = styled(Paper)(({ theme }) => ({
   textAlign: 'center',
   color: theme.palette.text.secondary,
 }));
+const { themeStretch } = useSettingsContext();
 type Props = {};
-function ModelsContainer({}: Props) {
-  const { themeStretch } = useSettingsContext();
   return (
-    <>
-      <head>
-        <title>Vehicle - Models</title>
-      </head>
-
+    <React.Fragment>
       <Container maxWidth={themeStretch ? false : 'lg'}>
         <Box sx={{ display: 'flex' }}>
           <Box sx={{ flexGrow: 1 }}>
             <Typography variant="h4" component="h4">
-              Vehicle Model
+              Vehicle Models
             </Typography>
           </Box>
 
           <Box sx={{}}>
-            <Button variant="contained">New Models</Button>
+            <Button variant="outlined" onClick={handleClickOpen}>
+              Add Models
+            </Button>
           </Box>
         </Box>
       </Container>
-    </>
+
+      <Dialog fullWidth={fullWidth} maxWidth={maxWidth} open={open} onClose={handleClose}>
+        <DialogTitle>Optional sizes</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            You can set my maximum width and whether to adapt or not.
+          </DialogContentText>
+          <Box
+            noValidate
+            component="form"
+            sx={{
+              display: 'flex',
+              flexDirection: 'column',
+              m: 'auto',
+              width: 'fit-content',
+            }}
+          >
+            <FormControl sx={{ mt: 2, minWidth: 120 }}>
+              <InputLabel htmlFor="max-width">maxWidth</InputLabel>
+              <Select
+                autoFocus
+                value={maxWidth}
+                onChange={handleMaxWidthChange}
+                label="maxWidth"
+                inputProps={{
+                  name: 'max-width',
+                  id: 'max-width',
+                }}
+              >
+                <MenuItem value={false as any}>false</MenuItem>
+                <MenuItem value="xs">xs</MenuItem>
+                <MenuItem value="sm">sm</MenuItem>
+                <MenuItem value="md">md</MenuItem>
+                <MenuItem value="lg">lg</MenuItem>
+                <MenuItem value="xl">xl</MenuItem>
+              </Select>
+            </FormControl>
+            <FormControlLabel
+              sx={{ mt: 1 }}
+              control={<Switch checked={fullWidth} onChange={handleFullWidthChange} />}
+              label="Full width"
+            />
+          </Box>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Close</Button>
+        </DialogActions>
+      </Dialog>
+    </React.Fragment>
   );
 }
-
-export default ModelsContainer;
